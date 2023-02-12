1995ae966559a684f1bc38568cddcd9522cb3967
vishalvedavyaspeddagopu <peddagop@usc.edu>
2023-02-11 17:40:14 -0800

    Added acceleration to the maze

--- Assets/Scripts/TiltMaze.cs
+++ Assets/Scripts/TiltMaze.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TiltMaze : MonoBehaviour
 { 
     public float speed;
+    public float minSpeed;
+    public float maxSpeed;
+    public float acceleration;
     public Vector3 customPivot;
 
     void Start()
@@ -17,14 +20,32 @@ public class TiltMaze : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(customPivot, Vector3.forward, speed * Time.deltaTime);
+            if (speed < maxSpeed)
+            {
+                speed += acceleration * Time.deltaTime;
+                transform.RotateAround(customPivot, Vector3.forward, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.RotateAround(customPivot, Vector3.forward, speed * Time.deltaTime);
+            }
         }
-      
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(customPivot, -Vector3.forward, speed * Time.deltaTime);
+            if (speed < maxSpeed)
+            {
+                speed += acceleration * Time.deltaTime;
+                transform.RotateAround(customPivot, -Vector3.forward, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.RotateAround(customPivot, -Vector3.forward, speed * Time.deltaTime);
+            }
         }
-
+        else
+        {
+            speed = minSpeed;
+        }
     }
 
 }
