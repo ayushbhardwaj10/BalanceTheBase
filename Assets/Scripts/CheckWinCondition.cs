26a67def9dfb04930314b43036b906757276b3a1
Sreeyuktha Hebbur Sathyendra Babu <hebbursa@usc.edu>
2023-02-12 21:43:44 -0800

    analytics added

--- Assets/Scripts/CheckWinCondition.cs
+++ Assets/Scripts/CheckWinCondition.cs
@@ -9,6 +9,15 @@ public class CheckWinCondition : MonoBehaviour
     List <GameObject> currentCollisions = new List <GameObject> ();
     public GameObject winningPopup;
 
+    DateTime startTime, endTime;
+    
+
+    void Start()
+    {
+        startTime = DateTime.Now;
+    }
+
+
     void OnCollisionEnter2D(Collision2D collision){
        if("BlueBall".Equals(collision.gameObject.tag) || "RedBall".Equals(collision.gameObject.tag)){
        currentCollisions.Add(collision.gameObject);
@@ -20,16 +29,20 @@ public class CheckWinCondition : MonoBehaviour
        GameObject.FindGameObjectsWithTag("RedBall").Length)) &&
        (GameObject.FindGameObjectsWithTag("BlueBall").Length == GameObject.FindGameObjectsWithTag("RedBall").Length )) {
             
-            string name = SceneManager.GetActiveScene().name;
-            Debug.Log("Scene "+ name);
-            string[] levelSplit = name.Split("_");
-            Debug.Log(levelSplit[2]);
+
+            string levelName = SceneManager.GetActiveScene().name;
+            string []levelSplit = levelName.Split("_");
+
             int l = Int32.Parse(levelSplit[2]);
             l++;
             Debug.Log("You Win");
-
-            //Debug.Log(levelSplit.Length);
-            SceneManager.LoadScene("Level_0_" + l.ToString());
+            endTime = DateTime.Now;
+            AnalyticsManager._instance.analytics_time_takenn(levelName, (int)(endTime - startTime).TotalSeconds, "Win");
+            if (l < 5)
+            {
+                SceneManager.LoadScene("Level_0_" + l.ToString());
+            }
+            
 
 
             //winningPopup.SetActive(true);
