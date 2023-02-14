771c420dd16c50950b069e054cb4a01d4d655eb2
Ayush Bhardwaj <40769053+ayushbhardwaj10@users.noreply.github.com>
2023-02-13 16:28:02 -0800

    UI issues for goal and guidance text resolved

--- Assets/Scripts/CheckLosingCondition.cs
+++ Assets/Scripts/CheckLosingCondition.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class CheckLosingCondition : MonoBehaviour
 {
     public GameObject losingPopup;
+
+    public void DisableLoosingPopup()
+    {
+        Debug.Log("close popup loose");
+        losingPopup.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
