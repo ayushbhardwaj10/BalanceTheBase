5d9527448d13510e34b93c347070f3ec63031f4d
RamyaSubramanianD <ramyas@usc.edu>
2023-02-20 19:04:11 -0800

    Added blink and pink wall creation feature for the new maze

--- Assets/Scripts/CreatePinkWall.cs
+++ Assets/Scripts/CreatePinkWall.cs
@@ -2,37 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CreatePinkWall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // Color pinkColor = new Vector4(221, 108, 190,1.0f);
-    Color pinkColor = new Vector4(0.7830189f, 0.1578784f, 0.1071111f,1.0f);
-    bool check = false;
-    public float speed;
+   // Start is called before the first frame update
+   public float speed;
+   public GameObject prefab;
 
 
-     void Update()
-    {
-        if(check){
-            if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
-        }
-      
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
-        }
-        }
-        
-    }
+   void Start(){
+       prefab = GameObject.Find("Pink Wall");
+   }
 
-    void OnCollisionEnter2D(Collision2D collision){
-        
-        if("PinkBallBlueBall".Equals(collision.gameObject.tag) || "PinkBallRedBall".Equals(collision.gameObject.tag)){
-        gameObject.GetComponent<SpriteRenderer> ().color = pinkColor;
-        check = true;
-        }
 
-    }
+   void OnCollisionEnter2D(Collision2D collision){
+
+
+       if("PinkBall_BlueBall".Equals(collision.gameObject.tag) || "PinkBall_RedBall".Equals(collision.gameObject.tag)){
+       var go = Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
+       go.transform.parent = gameObject.transform.parent;
+       go.transform.localScale = gameObject.transform.localScale;
+       Destroy(gameObject);
+       }
+
+
+   }
 }
