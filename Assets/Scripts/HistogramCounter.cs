1aeed18472c2b79bbe05831810b31c7a45462d67
Aastha Chowdhary <aasthac@usc.edu>
2023-02-27 14:25:29 -0800

    Added text blue equal red

--- Assets/Scripts/HistogramCounter.cs
+++ Assets/Scripts/HistogramCounter.cs
@@ -7,10 +7,15 @@ public class HistogramCounter : MonoBehaviour
     GameObject redBar;
     GameObject blueBar;
     [SerializeField] private bool increaseFromTop = true;
+    [SerializeField] private Canvas canvas;
     [SerializeField] private SpriteRenderer sprite1;
     [SerializeField] private SpriteRenderer sprite2;
     [SerializeField] private SpriteRenderer sprite3;
     [SerializeField] private int sortingOrder = 0;
+
+    public GameObject BText;
+    public GameObject EqualText;
+    public GameObject RText;
 
     int redCount;
     int blueCount;
@@ -26,19 +31,18 @@ public class HistogramCounter : MonoBehaviour
     {
         redCount = 0;
         blueCount = 0;
-        sprite1.sortingOrder = sortingOrder;
+        canvas.sortingOrder = sortingOrder + 2;
+        sprite1.sortingOrder = sortingOrder + 1;
+        sprite2.sortingOrder = sortingOrder;
         sprite3.sortingOrder = sortingOrder;
-        sprite2.sortingOrder = sortingOrder + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(redCount);
         int direction = increaseFromTop ? 1 : -1;
         if (redCount < GameObject.FindGameObjectsWithTag("RedBall").Length)
         {
-            Debug.Log("came inside red increase");
             redBar.GetComponent<SpriteRenderer>().transform.localScale = new Vector3(redBar.GetComponent<SpriteRenderer>().transform.localScale.x,
                                                                     redBar.GetComponent<SpriteRenderer>().transform.localScale.y + (GameObject.FindGameObjectsWithTag("RedBall").Length * 0.5f * direction),
                                                                     redBar.GetComponent<SpriteRenderer>().transform.localScale.z);
@@ -63,6 +67,22 @@ public class HistogramCounter : MonoBehaviour
                                                                      blueBar.GetComponent<SpriteRenderer>().transform.localScale.y - (GameObject.FindGameObjectsWithTag("BlueBall").Length * 0.5f * direction),
                                                                      blueBar.GetComponent<SpriteRenderer>().transform.localScale.x);
             blueCount = GameObject.FindGameObjectsWithTag("BlueBall").Length;
+        }
+
+        Debug.Log("Red count: " + redCount);
+        Debug.Log("Blue count: " + blueCount);
+
+
+        if (blueCount == redCount)
+        {
+            BText.SetActive(true);
+            EqualText.SetActive(true);
+            RText.SetActive(true);
+        } else
+        {
+            BText.SetActive(false);
+            EqualText.SetActive(false);
+            RText.SetActive(false);
         }
     }
 }
