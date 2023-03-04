094b3ffc32da0021726d90f0f5b367a6e84c3f05
Aastha Chowdhary <aasthac@usc.edu>
2023-03-03 18:08:19 -0800

    Resolved merge conflict

--- Assets/Scripts/CheckWinCondition.cs
+++ Assets/Scripts/CheckWinCondition.cs
@@ -10,11 +10,19 @@ public class CheckWinCondition : MonoBehaviour
     public GameObject winningPopup;
 
     DateTime startTime, endTime;
-    
+
+    [SerializeField] private SpriteRenderer sprite1;
+    [SerializeField] private SpriteRenderer sprite2;
+    [SerializeField] private SpriteRenderer sprite3;
+    [SerializeField] private int sortingOrder = 0;
 
     void Start()
     {
         startTime = DateTime.Now;
+
+        sprite1.sortingOrder = sortingOrder;
+        sprite2.sortingOrder = sortingOrder + 1;
+        sprite3.sortingOrder = sortingOrder + 1;
     }
 
 
