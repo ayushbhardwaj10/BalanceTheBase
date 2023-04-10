9c4f3fa10f4cbef3688bf31044695cd840a9df9f
Aastha Chowdhary <aasthac@usc.edu>
2023-04-09 19:49:59 -0700

    Minor update

--- Assets/Scripts/CheckLosingConditionBoxRegion.cs
+++ Assets/Scripts/CheckLosingConditionBoxRegion.cs
@@ -41,7 +41,16 @@ public class CheckLosingConditionBoxRegion : MonoBehaviour
 
         Debug.Log("BlueCount: " + blueCount);
         Debug.Log("RedCount: " + redCount);
-        LossChecker();
+        if (!lostStatus && !GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
+            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount)
+        {
+            LossChecker();
+        }
+        if (!lostStatus && redCount == 0 && blueCount == 0)
+        {
+            Debug.Log("In here");
+            LossChecker();
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -58,36 +67,39 @@ public class CheckLosingConditionBoxRegion : MonoBehaviour
         }
         Debug.Log("BlueCount: " + blueCount);
         Debug.Log("RedCount: " + redCount);
-        LossChecker();
+        if (!lostStatus && !GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
+            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount) {
+            LossChecker();
+        }
+        if (!lostStatus && redCount == 0 && blueCount == 0)
+        {
+            LossChecker();
+        }
     }
 
     void LossChecker()
-    {
-        if (!lostStatus && ((!GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
-            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount) || (redCount == 0 && blueCount == 0)))
-        {
-            lostStatus = true;
+    { 
+        lostStatus = true;
 
-            Debug.Log("YOU LOSE!");
-            losingPopup.SetActive(true);
-            // this.enabled = false; //added to get out of Update - IMPORTANT
-            endTime = DateTime.Now;
-            int time_taken = (int)(endTime - startTime).TotalSeconds;
-            Debug.Log("time taken in Losing Condition" + time_taken);
-            int user_rating = GamesManager._instance.calculate_user_ratings(GamesManager.LOST, levelName, time_taken);
-            Debug.Log("User rating in Losing Condition " + user_rating);
-            //GetComponent<StarHandler>().starsAcheived(user_rating);            
-            //Debug.Log("setting up Losing Condition pop-up");
+        Debug.Log("YOU LOSE!");
+        losingPopup.SetActive(true);
+        // this.enabled = false; //added to get out of Update - IMPORTANT
+        endTime = DateTime.Now;
+        int time_taken = (int)(endTime - startTime).TotalSeconds;
+        Debug.Log("time taken in Losing Condition" + time_taken);
+        int user_rating = GamesManager._instance.calculate_user_ratings(GamesManager.LOST, levelName, time_taken);
+        Debug.Log("User rating in Losing Condition " + user_rating);
+        //GetComponent<StarHandler>().starsAcheived(user_rating);            
+        //Debug.Log("setting up Losing Condition pop-up");
 
-            RestartButton.prev_level = SceneManager.GetActiveScene().name;
+        RestartButton.prev_level = SceneManager.GetActiveScene().name;
 
-            Debug.Log("Prev scene lost " + RestartButton.prev_level);
+        Debug.Log("Prev scene lost " + RestartButton.prev_level);
 
-            //Analytics for time taken
-            AnalyticsManager._instance.analytics_time_takenn(levelName, time_taken, GamesManager.LOST, RestartButton.isRestartClicked);
+        //Analytics for time taken
+        AnalyticsManager._instance.analytics_time_takenn(levelName, time_taken, GamesManager.LOST, RestartButton.isRestartClicked);
 
-            //Analytics for user ratings
-            AnalyticsManager._instance.analytics_user_ratings(levelName, time_taken, user_rating, GamesManager.LOST);
-        }
+        //Analytics for user ratings
+        AnalyticsManager._instance.analytics_user_ratings(levelName, time_taken, user_rating, GamesManager.LOST);
     }
 }
