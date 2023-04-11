3ecea06d94c3c7febca5ef9e2565a5c3ec1cdc87
Aastha Chowdhary <aasthac@usc.edu>
2023-04-10 22:02:36 -0700

    Fixed losing logic

--- Assets/CheckLosingConditionBoxRegion.cs
+++ Assets/CheckLosingConditionBoxRegion.cs
@@ -26,6 +26,8 @@ public class CheckLosingConditionBoxRegion : MonoBehaviour
         lostStatus = false;
         startTime = DateTime.Now;
         levelName = SceneManager.GetActiveScene().name;
+
+        InvokeRepeating("LossChecker", 0.0f, 1.0f);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -41,16 +43,6 @@ public class CheckLosingConditionBoxRegion : MonoBehaviour
 
         Debug.Log("BlueCount: " + blueCount);
         Debug.Log("RedCount: " + redCount);
-        if (!lostStatus && !GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
-            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount)
-        {
-            LossChecker();
-        }
-        if (!lostStatus && redCount == 0 && blueCount == 0)
-        {
-            Debug.Log("In here");
-            LossChecker();
-        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -67,40 +59,34 @@ public class CheckLosingConditionBoxRegion : MonoBehaviour
         }
         Debug.Log("BlueCount: " + blueCount);
         Debug.Log("RedCount: " + redCount);
-        if (!lostStatus && !GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
-            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount)
-        {
-            LossChecker();
-        }
-        if (!lostStatus && redCount == 0 && blueCount == 0)
-        {
-            LossChecker();
-        }
     }
 
     void LossChecker()
     {
-        lostStatus = true;
+        if (!lostStatus && ((!GameObject.FindWithTag("BlueSplitterTriangle") && !GameObject.FindWithTag("RedSplitterTriangle") &&
+            !GameObject.FindWithTag("BlinkingSplitter") && redCount != blueCount) || (redCount == 0 && blueCount == 0))) {
+            lostStatus = true;
 
-        Debug.Log("YOU LOSE!");
-        losingPopup.SetActive(true);
-        // this.enabled = false; //added to get out of Update - IMPORTANT
-        endTime = DateTime.Now;
-        int time_taken = (int)(endTime - startTime).TotalSeconds;
-        Debug.Log("time taken in Losing Condition" + time_taken);
-        int user_rating = GamesManager._instance.calculate_user_ratings(GamesManager.LOST, levelName, time_taken);
-        Debug.Log("User rating in Losing Condition " + user_rating);
-        //GetComponent<StarHandler>().starsAcheived(user_rating);            
-        //Debug.Log("setting up Losing Condition pop-up");
+            Debug.Log("YOU LOSE!");
+            losingPopup.SetActive(true);
+            // this.enabled = false; //added to get out of Update - IMPORTANT
+            endTime = DateTime.Now;
+            int time_taken = (int)(endTime - startTime).TotalSeconds;
+            Debug.Log("time taken in Losing Condition" + time_taken);
+            int user_rating = GamesManager._instance.calculate_user_ratings(GamesManager.LOST, levelName, time_taken);
+            Debug.Log("User rating in Losing Condition " + user_rating);
+            //GetComponent<StarHandler>().starsAcheived(user_rating);            
+            //Debug.Log("setting up Losing Condition pop-up");
 
-        RestartButton.prev_level = SceneManager.GetActiveScene().name;
+            RestartButton.prev_level = SceneManager.GetActiveScene().name;
 
-        Debug.Log("Prev scene lost " + RestartButton.prev_level);
+            Debug.Log("Prev scene lost " + RestartButton.prev_level);
 
-        //Analytics for time taken
-        AnalyticsManager._instance.analytics_time_takenn(levelName, time_taken, GamesManager.LOST, RestartButton.isRestartClicked);
+            //Analytics for time taken
+            AnalyticsManager._instance.analytics_time_takenn(levelName, time_taken, GamesManager.LOST, RestartButton.isRestartClicked);
 
-        //Analytics for user ratings
-        AnalyticsManager._instance.analytics_user_ratings(levelName, time_taken, user_rating, GamesManager.LOST);
+            //Analytics for user ratings
+            AnalyticsManager._instance.analytics_user_ratings(levelName, time_taken, user_rating, GamesManager.LOST);
+        }
     }
 }
