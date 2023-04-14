f84472790533f37fd1a85a9c30f6cea1691fa1b6
Aastha Chowdhary <aasthac@usc.edu>
2023-04-13 23:44:49 -0700

    Added condition for texts

--- Assets/Scripts/MessageOrder51.cs
+++ Assets/Scripts/MessageOrder51.cs
@@ -8,6 +8,7 @@ public class MessageOrder51 : MonoBehaviour
     public TextMeshProUGUI instruction1;
     public TextMeshProUGUI instruction2;
     public TextMeshProUGUI instruction3;
+    public int flag = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,27 @@ public class MessageOrder51 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K) && flag == 1)
         {
             instruction1.enabled = false;
+
+            flag = 2;
+        }
+
+        if (flag == 2)
+        {
+            instruction2.enabled = true;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                instruction2.enabled = false;
+                flag = 3;
+            }
+        }
+
+        if (flag == 3)
+        {
+            instruction3.enabled = true;
         }
     }
 }
