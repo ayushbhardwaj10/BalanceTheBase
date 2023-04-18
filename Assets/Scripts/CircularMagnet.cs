3d9e02592dfb046884db892e3af13dbfdf1c23ba
Sreeyuktha Hebbur Sathyendra Babu <hebbursa@usc.edu>
2023-04-17 23:40:27 -0700

    level_6_1 removed white walls,checked loose pop up

--- Assets/Scripts/CircularMagnet.cs
+++ Assets/Scripts/CircularMagnet.cs
@@ -4,6 +4,44 @@ using UnityEngine;
 
 public class CircularMagnet : MonoBehaviour
 {
+
+
+    // *********************************** This is wrt Keyboard Control ********************************************** 
+
+    //public float moveSpeed = 5f; // the speed at which the object moves
+    //public float boundaryRadius = 5f; // the radius of the circular boundary
+
+    //private Vector2 centerPosition; // the center position of the circular boundary
+
+
+    //void Start()
+    //{
+    //    centerPosition = transform.position; // set the center position to the initial position of the object
+    //}
+
+    //void Update()
+    //{
+    //    // get the horizontal and vertical input from the keyboard
+    //    float horizontalInput = Input.GetAxisRaw("Horizontal");
+    //    float verticalInput = Input.GetAxisRaw("Vertical");
+    //    // calculate the movement direction based on the keyboard input
+    //    Vector2 movementDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+    //    // calculate the target position based on the movement direction, the speed, and the center position
+    //    Vector2 targetPosition = centerPosition + movementDirection * boundaryRadius;
+
+    //    // move the object towards the target position
+    //    transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+    //    // clamp the object within the circular boundary
+    //    float distanceFromCenter = Vector2.Distance(centerPosition, transform.position);
+    //    if (distanceFromCenter > boundaryRadius)
+    //    {
+    //        transform.position = (Vector3)centerPosition + ((Vector3)(transform.position) - (Vector3)(centerPosition)).normalized * boundaryRadius;
+    //    }
+    //}
+
+
     public float radius = 5f; // the radius within which the object can be moved
     private Vector3 originalPos; // the object's original position
     private Vector3 mousePos; // the current position of the mouse
@@ -20,21 +58,13 @@ public class CircularMagnet : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0; // make sure the z-coordinate is zero
 
-
         // calculate the distance between the mouse and the original position of the object
         float distance = Vector3.Distance(originalPos, mousePos);
 
         // check if the mouse is within the radius
-        //if (distance == radius)
-        //{
-        //    newPos = mousePos; // set the new position to the mouse position
-        //    Debug.Log("1");
-        //}
-        //else
-        //{
+        
             newPos = originalPos + (mousePos - originalPos).normalized * radius; // set the new position to the edge of the radius
-            
-       // }
+        
 
         // move the object to the new position
         transform.position = newPos;
