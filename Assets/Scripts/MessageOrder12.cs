b193a37877fb180a36e3bf0a6b24a12cd3c14d0b
vishalvedavyaspeddagopu <peddagop@usc.edu>
2023-04-23 13:48:25 -0700

    Level 1-2 dynamic instructions

--- Assets/Scripts/MessageOrder12.cs
+++ Assets/Scripts/MessageOrder12.cs
@@ -7,6 +7,7 @@ public class MessageOrder12 : MonoBehaviour
 {
     public TextMeshProUGUI instruction1;
     public TextMeshProUGUI instruction2;
+    public TextMeshProUGUI instruction3;
     public int flag = 1;
 
     // Start is called before the first frame update
@@ -14,15 +15,43 @@ public class MessageOrder12 : MonoBehaviour
     {
         instruction1.enabled = true;
         instruction2.enabled = false;
+        instruction3.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && flag == 1)
+        if (Input.GetKeyDown(KeyCode.S) && flag == 2)
         {
-            instruction1.enabled = false;
-            instruction2.enabled = true;
+            instruction2.enabled = false;
+            instruction3.enabled = true;
+            flag++;
+
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    public void MoveToInstruction2()
+    {
+        instruction1.enabled = false;
+        instruction2.enabled = true;
+        flag++;
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (flag == 3)
+        {
+            yield return new WaitForSeconds(3f);
+            float duration = 2f;
+            float alpha = instruction3.color.a;
+
+            while (instruction3.color.a > 0f)
+            {
+                alpha -= Time.deltaTime / duration;
+                instruction3.color = new Color(instruction3.color.r, instruction3.color.g, instruction3.color.b, alpha);
+                yield return null;
+            }
             flag++;
         }
     }
